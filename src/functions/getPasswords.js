04f361b434e05687d40dcd3c99f7f4ad2edf4340
Bachir Sbinati <sbinatibachir@gmail.com>
2023-10-19 09:28:37 +0200

    Replace single-password fetching with endpoint for getting all passwords and domains directly

--- src/functions/getPasswords.js
+++ src/functions/getPasswords.js
@@ -2,8 +2,9 @@ import { db } from "@/services/db";
 import { sendResponse, sendError } from "@/responses";
 import { checkPermission } from "@/middleware/permission";
 import { middyfy } from "@/services/middify";
+import { decryptPassword } from "@/models/user";
 
-const getDomains = async (event) => {
+const getPasswords = async (event) => {
   try {
     const params = {
       TableName: process.env.TABLE_NAME,
@@ -15,13 +16,14 @@ const getDomains = async (event) => {
 
     const { Items } = await db.query(params).promise();
 
-    const domains = Items.filter(item => item.SK !== event.user).map(({ SK }) => SK);
+    const domains = Items
+      .filter(item => item.SK !== event.user)
+      .map(item => ({ domain: item.SK, password: decryptPassword(item.password) }));
 
     return sendResponse(200, { success: true, domains });
   } catch (error) {
-    console.error(error)
-    return sendError(500, { success: false, error: 'Could not fetch domains' });
+    return sendError(500, { success: false, error: error.message });
   }
 }
 
-export const handler = middyfy(getDomains).use(checkPermission);
+export const handler = middyfy(getPasswords).use(checkPermission);
